fe913b5626bf0558c551802e42d8fa79399d434f
scnerd <scnerd@gmail.com>
2018-01-05 17:45:56 -0500

    Major upgrades. Added proxy in front of pythonathon server, and use wsgi instead of direct http. Added jupyterhub sibling server so competitors don't have to install their own dev environments. Added multi-db postgres to allow jupyterhub to use the same db server.

---
+++ proxy/start.sh
@@ -0,0 +1,3 @@
+envsubst '$VIRTUAL_HOST,$MAIN_HOST,$MAIN_PORT,$HUB_HOST,$HUB_PORT,$HUB_PATH' < /etc/nginx/conf.d/pythonathon.template > /etc/nginx/conf.d/default.conf &&
+cat /etc/nginx/conf.d/default.conf &&
+nginx -g 'daemon off;'
