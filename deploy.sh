dade73999a41e2b8eb1a5c9ade0fe6104a673b22
Ubuntu <ubuntu@ip-172-31-93-68.ec2.internal>
2018-01-29 21:32:26 +0000

    Minor tweaks to fix production launcher

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 docker-compose pull
-docker-compose -f docker-compose.yml -f production.yml up -d
+#docker-compose -f docker-compose.yml -f production.yml up -d
+
+docker-compose -f docker-compose.yml -f production.yml config > full_production.yml
+docker stack deploy --compose-file full_production.yml pythonathon
