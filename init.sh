27c335e7f1dfbbd8ff81d3b4d64489b25400c746
dmaxson <dmaxson@ltsnet.net>
2018-02-05 17:45:32 -0500

    Enabled restoring database from backup, using a backup file in ctf_data/restore

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-makedir -p secrets ctf_data/db_backups ctf_data/files oauth db_data
+mkdir -p secrets ctf_data/db_backups ctf_data/files ctf_data/restore oauth db_data
 
 cat /dev/urandom | tr -dc 'a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_' | fold -w 32 | head -n 1 > secrets/db_password
