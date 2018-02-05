22e180a3f4f3e7a429ba4663af1ab206b946edf0
Ubuntu <ubuntu@ip-172-31-93-68.ec2.internal>
2018-02-05 22:46:17 +0000

    Resolved merge conflict

--- init.sh
+++ init.sh
@@ -4,4 +4,6 @@ set -ex
 
 mkdir -p secrets ctf_data/db_backups ctf_data/files ctf_data/restore oauth db_data
 
+if [[ ! -e secrets/db_password ]]; then
 cat /dev/urandom | tr -dc 'a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_' | fold -w 32 | head -n 1 > secrets/db_password
+fi
